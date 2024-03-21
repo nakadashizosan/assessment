96a3a91e0a0844a833d312e773357b9b6e7cd8b1
nakadashizosan <164288487+nakadashizosan@users.noreply.github.com>
2024-03-21 15:52:24 +0900

    boostrapで装飾

--- assessment.js
+++ assessment.js
@@ -5,50 +5,64 @@ const resultDivision = document.getElementById('result-area');
 const tweetDivision = document.getElementById('tweet-area');
 
 assessmentButton.addEventListener(
-    'click',
-    () => {
-        const userName = userNameInput.value;
-         if (userName.length === 0) {
-            // 名前がからの時は処理を終了する
-            return;
-         }
-
-        // 診断結果表示エリアの作成
-        resultDivision.innerText = '';
-        const header = document.createElement('h3');
-        header.innerText = '診断結果';
-        resultDivision.appendChild(header);
-
-        const paragraph = document.createElement('p');
-        const result = assessment(userName);
-        paragraph.innerText = result;
-        resultDivision.appendChild(paragraph);
-
-        // ツイートエリアの作成
-        tweetDivision.innerText = '';
-        const anchor = document.createElement('a');
-        const hrefvalue =
-            'https://twitter.com/intent/tweet?button_hashtag=' +
-            encodeURIComponent('あなたのいいところ') +
-            '&ref_src=twsrc%5Etfw';
-        
-        anchor.setAttribute('href',hrefvalue);
-        anchor.setAttribute('class','twitter-hashtag-button');
-        anchor.setAttribute('data-text',result);
-        anchor.innerText = 'Tweet #あなたのいいところ';
-
-        tweetDivision.appendChild(anchor);
-
-
-        const script = document.createElement('script');
-        script.setAttribute('src','https://platform.twitter.com/widgets.js');
-        tweetDivision.appendChild(script);
+  'click',
+  () => {
+    const userName = userNameInput.value;
+    if (userName.length === 0) {
+      // 名前が空の時は処理を終了する
+      return;
     }
+
+    // 診断結果表示エリアの作成
+    resultDivision.innerText = '';
+    
+  // headerDivision の作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class', 'card-header text-bg-primary');
+  headerDivision.innerText = '診断結果';
+
+  // bodyDivision の作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
+
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
+  const result = assessment(userName);
+  paragraph.innerText = result;
+  bodyDivision.appendChild(paragraph);
+
+  // resultDivision に Bootstrap のスタイルを適用する
+  resultDivision.setAttribute('class', 'card');
+
+  // headerDivision と bodyDivision を resultDivision に差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
+
+    // ツイートエリアの作成
+    tweetDivision.innerText = '';
+    const anchor = document.createElement('a');
+    const hrefValue =
+      'https://twitter.com/intent/tweet?button_hashtag=' +
+      encodeURIComponent('あなたのいいところ') +
+      '&ref_src=twsrc%5Etfw';
+  
+    anchor.setAttribute('href', hrefValue);
+    anchor.setAttribute('class', 'twitter-hashtag-button');
+    anchor.setAttribute('data-text', result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
+  
+    tweetDivision.appendChild(anchor);
+
+
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    tweetDivision.appendChild(script);
+  }
 );
 
 userNameInput.addEventListener(
   'keydown',
-  (event) => {
+  event => {
     if(event.code === 'Enter') {
       assessmentButton.dispatchEvent(new Event('click'))
     }
@@ -102,7 +116,7 @@ function test() {
   console.log('太郎');
   console.assert(
     assessment('太郎') ===
-      '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
 
@@ -110,7 +124,7 @@ function test() {
   console.log('次郎');
   console.assert(
     assessment('次郎') ===
-      '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
+    '次郎のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる次郎が皆から評価されています。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
 
@@ -118,10 +132,10 @@ function test() {
   console.log('花子');
   console.assert(
     assessment('花子') ===
-      '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
+    '花子のいいところはまなざしです。花子に見つめられた人は、気になって仕方がないでしょう。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
   );
-  
+
   console.log('診断結果の文章のテスト終了');
 
   console.log('同じ名前なら、同じ結果を出力することのテスト');
